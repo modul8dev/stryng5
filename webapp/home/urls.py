4f6e52caea4b04f448e0b65da2d5e28d820d4b69
Tomislav Sabados <tomislav.sabados@gmail.com>
2026-03-25 07:25:23 +0000

    Remove about screen

--- webapp/home/urls.py
+++ webapp/home/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import home, settings, about, article
+from .views import home, settings
 
 urlpatterns = [
     path("", home, name="home"),
     path("settings", settings, name="settings"),
-    path("about", about, name="about"),
-    path("article", article, name="article")
-
 ]
